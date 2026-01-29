f3b09b32dff59fd25b4928154e28d7d8a42f40b3
miranda-beatriz <miranda.beatrizcosta@gmail.com>
2026-01-28 22:26:19 -0300

    almost finishing

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
+using BlazingPizza.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+
+builder.Services.AddHttpClient();
+
+// registra o DbContext (do jeito que você já tentou)
+builder.Services.AddSqlite<PizzaStoreContext>("Data Source=pizza.db");
 
 var app = builder.Build();
 
@@ -16,5 +24,18 @@ app.UseRouting();
 app.MapRazorPages();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+// Se você não tem controllers, pode remover essa linha:
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
+// Initialize the database
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<PizzaStoreContext>();
+
+    if (db.Database.EnsureCreated())
+    {
+        SeedData.Initialize(db);
+    }
+}
 
 app.Run();
